8dca1bb71f42c8861634f716af6f48cd0fb67bb9
HyuzaNozomi <linglegendssix@gmail.com>
2025-11-19 11:50:49 +0800

    Feat <Prosedural>: Pratikum PBO

---
+++ Modul_1_Konsep_OOP/Prosedural.py
@@ -0,0 +1,14 @@
+# Pendekatan Prosedural
+def hitung_luas_persegi(sisi):
+    return sisi * sisi
+
+def hitung_luas_segitiga(sisi):
+    return 4 * sisi
+
+# Pemanggilan fungsi
+sisi = 5
+luas_persegi = hitung_luas_persegi(sisi)
+keliling_segitiga = hitung_luas_segitiga(sisi)
+
+print(f"Luas persegi: {luas_persegi}")
+print(f"Keliling segitiga: {keliling_segitiga}")    
